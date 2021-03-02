1a3a57f93e1554aa7c15e7f7d8916a4c2c489623
Paulo Diovani <paulo@diovani.com>
2021-03-02 20:09:53 -0300

    Translates help and fix typos

--- kultMoveBot.py
+++ kultMoveBot.py
@@ -29,39 +29,40 @@ gap = ''
 
 moves = all_moves.moves
 
-help=""
+help="""
+# Como usar:
 
-help += "# Usage:\n"
-help += "!move ? - displays this message\n"
-help += "!move help - displays this message\n"
-help += "!move info - displays bot code info\n"
-help += "!move xxx - roll to perform Move xxx\n"
-help += "!move xxx -1 - roll to perform Move xxx with negative modifier -1\n"
-help += "!move xxx +2 - roll to perform Move xxx with positive modifier +2\n"
-help += "# Moves:\n"
-help += "- Use the full move name (lower-case, no spaces) when rolling.\n"
-help += "  For example, to roll for 'Artistic Talent' with a +2 bonus use:\n" 
-help += "  !move artistictalent +2\n"
-help += "- Shortcuts are available for commonly used moves (see below).\n"
-help += "  For example, to roll for 'Avoid Harm' with a +1 bonus use:\n"
-help += "  !move ah +1\n"
-help += "# Move shortcuts:\n"
-help += "- Avoid Harm (ah): roll + Reflexes\n"
-help += "- Endure Injury (ei): roll + Fortitude - Harm (+ Armour)\n"
-help += "- Keep It Together (kit): roll + Willpower\n"
-help += "- Act Under Pressure (aup): roll + Coolness\n"
-help += "- Engage In Combat (eic): roll + Violence\n"
-help += "- Influence Other - NPC (ion): roll + Charisma\n"
-help += "- Influence Other - PC (iop): roll + Charisma\n"
-help += "- See Through the Illusion (sti): roll + Soul\n"
-help += "- Read A Person (rap): roll + Intuition\n"
-help += "- Observe A Situation (oas): roll + Perception\n"
-help += "- Investigate (inv): roll + Reason\n"
-help += "- Help Or Hinder (hoh): roll + Attribute\n"
-help += "- Non-standard move (non): roll + Modifier\n"
-help += "# Anomalies (full move name):\n"
-help += "- Influence Other - NPC: !move influenceothernpc\n" 
-help += "- Influence Other - PC: !move influenceotherpc\n" 
+!movimento ? - exibe esta mensagem
+!movimento help - exibe esta mensagem
+!movimento ajuda - exibe esta mensagem
+!movimento info - exibe informações do Bot
+!movimento xxx - realiza um movimento xxx
+!movimento xxx -1 - realiza um movimento xxx com um modificador negativo de -1
+!movimento xxx +2 - realiza um movimento xxx com um modificador positivo de +2
+
+Movimentos:
+- Use o nome completo do movimento (em minúsculas, sem espaços) ao jogar.
+  Por exemplo, para jogar 'Observar uma Situação' com um bônus de +2, use:
+  !movimento observarumasituacao +2
+- Atalhos estão disponíveis para os movimentos mais usados (veja abaixo).
+  Por exemplo, para jogar 'Evitar Ferimento' com um bônus de +1, use:
+  !movimento ef +1
+
+# Atalhos de movimentos:
+- Evitar Dano (ed): jogue + Reflexos
+- Suportar Ferimento (sf): jogue + Fortitude - Dano (+ Armadura)
+- Manter o Sangue-frio (msf): jogue + Força de Vontade
+- Agir Sob Pressão (asp): jogue + Sangue-frio
+- Participar do Combate (pdc): jogue + Violência
+- Influenciar um PdM (ipm): jogue + Carisma
+- Influenciar um Pj (ipj): jogue + Carisma
+- Ver Através da Ilusão (vai): jogue + Alma
+- Read A Person (rap): jogue + Intuição
+- Observar uma Situação (oas): jogue + Percepção
+- Investigar (inv): jogue + Razão
+- Ajudar ou Atrapalhar (aoa): jogue + Atributo
+- Movimento fora do padrão (mov): jogue + Modificador
+"""
 
 ## Get discord connection
 client = discord.Client()
@@ -72,34 +73,34 @@ async def on_message(message):
 
     ## Bot info
     info=""
-    info += "# KultMoveBot\n"
-    info += "Code available at: https://github.com/rpgmik/KultMoveBot\n"
+    info += "# KultMoveBotBr\n"
+    info += "Código-fonte disponível em: https://github.com/paulodiovani/kult-move-bot-ptbr"
     ## Collect server install info
-    info += "Currently running on " + str(len(list(client.guilds))) + " Discord guilds.\n"
+    info += "Atualmente rodando em " + str(len(list(client.guilds))) + " servidores Discord.\n"
 
     ## Converts move command to lowercase, making it
     ## case insensitive
     message.content=message.content.lower()
 
-    ## Respond if user sends "!move"
-    if message.content.startswith('!move'):
+    ## Respond if user sends "!movimento"
+    if message.content.startswith('!movimento'):
 
         ## Split into into "!move", the type of Move to undertake, the modifier (if any), and a comment (if any).
         bits = message.content.split(" ")
 
         dice = '```md\n'
         if len(bits)==1:
-            dice += "Please specify a Move (or use '!move ?' for help)"
+            dice += 'Favor, espeficique um Movimento (ou "!movimento ?" para ajuda)'
 
         if len(bits)>1:
-            if bits[1] in ["?", "help"]:
+            if bits[1] in ["?", "help", "ajuda"]:
                 dice += help
 
             elif bits[1] in ["info"]:
                 dice += info
 
             elif bits[1] not in moves:
-                dice += 'Please specify a Move (or "!move ?" for help)'
+                dice += 'Favor, espeficique um Movimento (ou "!movimento ?" para ajuda)'
 
             elif bits[1] in moves:
                 roll = [random.randint(1,10), random.randint(1,10)]
@@ -144,7 +145,7 @@ async def on_message(message):
 ## Write login details locally (i.e., on linux box where bot code is running)
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="Kult: !move ? for help"))
+    await client.change_presence(activity=discord.Game(name="Kult: !movimento ? para ajuda"))
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
